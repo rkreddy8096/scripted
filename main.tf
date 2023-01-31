be5549ad87b343a3ba2616703acacb6112c8a28b
rkreddy8096 <112272534+rkreddy8096@users.noreply.github.com>
2023-01-31 23:07:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,15 +6,12 @@ resource "null_resource" "webprovisoner" {
         user        = "jenkins"
         password    = "jenkins"
         port        = 22
-        private_key = file("~/.ssh/id_rsa")
-        host        = self.public_ip
+        host        = "localhost"
       }
-      
+
       inline = [
         "sudo apt update",
         "sudo apt install apache2 -y",
-        "sudo systemctl start apache2",
-        "sudo systemctl enable apache2"
-      ]
+       ]
     }
 }
